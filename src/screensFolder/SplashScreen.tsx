51fd67e940d9adb085ee70e3aba099ca3c9edc34
SukanyaT2022 <sukanyat2022a@gmail.com>
2024-12-12 19:58:43 -0500

    first

---
+++ src/screensFolder/SplashScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Icon from 'react-native-vector-icons/AntDesign';
+import { Text, View } from 'react-native'
+
+const SplashScreen = () => {
+  return (
+    <View>
+        <Text>Splash Screen</Text>
+        <Icon name="up" size={30} color="#900" />
+     </View>
+  )
+}
+
+export default SplashScreen
